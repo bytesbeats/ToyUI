a23f2298157d7d16861bf984e398485e8957dd87
guozichun <guotingchaopr@gmail.com>
2024-12-15 22:12:13 +0800

    :art: Improve code formatting and use single quotes for imports
    - Updated import statements to use single quotes consistently
    - Refactored `PageProps` type definition to remove unnecessary `Readonly`
    - Simplified function signature in `CookingPlan` component

--- src/app/[lang]/cooking-plan/page.tsx
+++ src/app/[lang]/cooking-plan/page.tsx
@@ -1,11 +1,10 @@
-import CookiePlanPage from "@app/components/Pages/cookie-plan";
-import Container from "@components/Container";
-import { PageProps } from "@constants/common.constant";
-import React from "react";
+import CookiePlanPage from '@app/components/Pages/cookie-plan';
+import Container from '@components/Container';
+import React from 'react';
 
-export default async function CookingPlan(_props: PageProps) {
+export default async function CookingPlan() {
   return (
-    <Container className="p-3">
+    <Container className='p-3'>
       <CookiePlanPage />
     </Container>
   );
--- src/constants/common.constant.ts
+++ src/constants/common.constant.ts
@@ -1,9 +1,9 @@
-import { Locales } from "@locales/next-i18next.config";
-import { SearchParams } from "next/dist/server/request/search-params";
-import { ReactNode } from "react";
+import { Locales } from '@locales/next-i18next.config';
+import { SearchParams } from 'next/dist/server/request/search-params';
+import { ReactNode } from 'react';
 
-export type PageProps = Readonly<{
+export type PageProps = {
   params: Promise<{ lang: Locales }>;
   searchParams?: SearchParams;
   children?: ReactNode;
-}>;
+};
